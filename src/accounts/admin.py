5db77f1fbaea0499dbaac10f47f9b095234c2e53
Sreeram Maram <sreeram@goexfx.com>
2020-05-05 10:01:57 +0530

    Admin page created for models

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+@admin.register(models.UserProfile)
+class AccountAdmin(admin.ModelAdmin):
+    fields          =   (  "user", "identity_number", "real_name", "contact_number", "timezone", "email")
+    list_display    = 	fields + ( "created_on", "updated_on" )
+
+    list_display_links  = list_display
+    model           =   models.UserProfile
+    extra           =   0
+
+
+@admin.register(models.ActivityPeriod)
+class ActivityPeriodAdmin(admin.ModelAdmin):
+    fields          =   (  "user", "start_time", "end_time")
+    list_display    = 	fields + ( "created_on", "updated_on" )
+
+    list_display_links  = list_display
+    model           =   models.ActivityPeriod
+    extra           =   0
--- src/accounts/models.py
+++ src/accounts/models.py
@@ -7,6 +7,9 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user                    = models.OneToOneField(User, on_delete=models.CASCADE, related_name = 'userprofile')
+    identity_number		 	= models.CharField(max_length = 127, blank=True, null=True)
+    real_name 				= models.CharField(max_length = 127, blank=True, null=True)
+    timezone 				= models.CharField(max_length = 127, blank=True, null=True)
     contact_number 			= models.CharField(max_length = 12, blank=True, null=True)
     email 					= models.CharField(max_length = 120, blank=True, null=True)
 
@@ -41,3 +44,6 @@ class ActivityPeriod(models.Model):
 
 	def __str__(self):
 		return f'{self.user.username} - {self.user.username}'
+
+
+
